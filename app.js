341c6d4367640a724b32c481252cabe8eb0d85ce
heipakchristinelam <hl913h@att.com>
2016-09-21 16:08:57 -0700

    adding jest for testing

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 module.exports = {
     add: function(num1, num2) {
         return num1 + num2;
+    },
+    mul: function(num1, num2, num3) {
+        return num1 * num2 * num3;
     }
 };
--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 require('chai').should();
+
+
 var pkg = require('./app');
+
 describe('pkg', function() {
     it('adds two numbers', function() {
         pkg.add(1, 1).should.equal(2);
     });
+    it('multiply three numbers', function() {
+        pkg.mul(2, 3, 1).should.equal(6);
+    })
 });
